8b8885fc0ad636bfb2e28ccd143d17b8079be602
R0mari024-dev <dev.r0mari0.24@gmail.com>
2026-04-10 12:37:25 +0300

    Доделанное меню
    Доделал все кнопки тоетст скилет

--- code-evidence/Scripts/MainMenu.cs
+++ code-evidence/Scripts/MainMenu.cs
@@ -10,11 +10,11 @@ public partial class MainMenu : CanvasLayer
 	{
 	}
 
-	// 
+	// Кнопки гавного Меню
 	private void _on_button_1_pressed()
 	{
 		GD.Print("Pess button Continue!");
-	
+	}
 	private void _on_button_2_pressed()
 	{
 		GD.Print("Pess button Start!");
@@ -37,24 +37,89 @@ public partial class MainMenu : CanvasLayer
 	private void _on_button_3_pressed()
 	{
 		GD.Print("Pess button Things!");
+
+		var group1 = GetNode<CanvasGroup>("Things");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
 	}
 	private void _on_button_4_pressed()
 	{
 		GD.Print("Pess button Leaflets!");
+
+		var group1 = GetNode<CanvasGroup>("Leaflets");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
 	}
 	private void _on_button_5_pressed()
 	{
 		GD.Print("Pess button Mini-game!");
+
+		var group1 = GetNode<CanvasGroup>("Mini Games");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
+
 	}
 	private void _on_button_6_pressed()
 	{
 		GD.Print("Pess button Settings!");
+
+		var group1 = GetNode<CanvasGroup>("Settings");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
+
 	}
 	private void _on_button_7_pressed()
 	{
 		GD.Print("Pess button Exit!");
 		GetTree().Quit();
 	}
+
+
+	// Кнопки выбора уровня Меню уровней
 	private void _on_button_exit_pressed()
 	{
 		GD.Print("Bake to menu");
@@ -73,9 +138,6 @@ public partial class MainMenu : CanvasLayer
 				group2.Visible = false;
 			}
 		}
-
-
-
 	private void _on_button_level_1_pressed()
 	{
 		GD.Print("Select level 1!");
@@ -83,5 +145,88 @@ public partial class MainMenu : CanvasLayer
 		
 	}
 
+
+	// Кнопки настроек Меню настроек
+	private void _on_buttonSettings_exit_pressed()
+	{
+		GD.Print("Bake to menu");
+		var group1 = GetNode<CanvasGroup>("Settings");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
+	}
+
+
+	// Меню Things
+	private void _on_button_exit_things_pressed()
+	{
+		GD.Print("Bake to menu");
+		var group1 = GetNode<CanvasGroup>("Things");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
+	}
+
+
+	// Кнопки листовок Меню Настроек
+	private void _on_button_exit_leaflets_pressed()
+	{
+		GD.Print("Bake to menu");
+		var group1 = GetNode<CanvasGroup>("Leaflets");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
+	}
+
+
+	// кнопки мини игр Меню Мини игр
+	private void _on_button_exit_mini_games_pressed()
+	{
+		GD.Print("Bake to menu");
+		var group1 = GetNode<CanvasGroup>("Mini Games");
+		var group2 = GetNode<CanvasGroup>("Menu");
+
+		if (group1.IsVisibleInTree())
+			{
+				group1.Visible = false;
+				group2.Visible = true;
+			}
+			
+		else
+			{
+				group1.Visible = true;
+				group2.Visible = false;
+			}
+	}
 
 }
